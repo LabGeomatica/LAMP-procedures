b028d00aece817669dc94d739b5bab33d15216e0
LabGeomatica <108334728+LabGeomatica@users.noreply.github.com>
2022-11-17 13:38:14 +0100

    Update script.py

--- script.py
+++ script.py
@@ -4,18 +4,19 @@ Program name: Unsaturated soil shear strenght
 
 Created on Fri Jun  3 14:21:52 2022
 @authors: Estelle Stefanini, Stefania Viaggio
-Estelle was student at the National School of Geographic Sciences (ENSG - Fr), during her internship in the Geomatic Lab at the University of Genova (UniGe - It)
-Stefania was doing her PhD in the Geomatic Lab at the University of Genova (UniGe - It)
-They both worked under the supervision of Proff. Rossella Bovolenta and Bianca Federici, UniGe. 
+Estelle was student at the National School of Geographic Sciences (ENSG - France); she developed the program during her internship in the Geomatic Lab at the University of Genova (UniGe - Italy)
+Stefania was PhD candidate in the Geomatic Lab at the University of Genova (UniGe - It)
+They both worked under the supervision of Profs Rossella Bovolenta and Bianca Federici, at UniGe. 
 
 Copyright (C) 2022 Stefanini & Viaggio
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License along with this program.  If not, see https://www.gnu.org/licenses/.
 
-If you need to contact us: stefania.viaggio et edu.unige.it, rossella.bovolenta et unige.it, bianca.federici et unige.it
+If you need to contact us: stefania.viaggio at edu.unige.it, rossella.bovolenta at unige.it, bianca.federici at unige.it
+
 
 """
 
